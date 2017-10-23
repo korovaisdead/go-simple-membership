86bbb2d9b29b08f1eb0f3a7838972d9b67f9d3ee
Pavel Shtanko <pavel.shtanko@gmail.com>
2017-10-23 11:19:45 +0200

    Adde login endpoint

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ func main() {
 	var router *mux.Router
 	router = mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/register", RegisterHandler).Methods(http.MethodPost)
-	router.HandleFunc("/authenticate", RegisterHandler).Methods(http.MethodPost)
+	router.HandleFunc("/authenticate", LoginHandler).Methods(http.MethodPost)
 
 	if err := http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, router)); err != nil {
 		panic(err)
@@ -31,8 +31,8 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	registerModel := RegisterModel{}
-	if err = json.Unmarshal(body, &registerModel); err != nil {
+	registerModel := &RegisterModel{}
+	if err = json.Unmarshal(body, registerModel); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
@@ -55,6 +55,28 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+//LoginHandler represens the handler of the login action
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	loginModel := &LoginModel{}
+	if err = json.Unmarshal(body, loginModel); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if len(strings.TrimSpace(loginModel.Email)) == 0 || len(strings.TrimSpace(loginModel.Password)) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
 //RegisterModel structure is represent the entoty to add
 type RegisterModel struct {
 	Firstname            string `json:"firstname"`
@@ -63,4 +85,10 @@ type RegisterModel struct {
 	Phone                string `json:"phone"`
 	Password             string `json:"password"`
 	PasswordConfirmation string `json:"confirmation"`
+}
+
+//LoginModel represents the model for the login action
+type LoginModel struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
