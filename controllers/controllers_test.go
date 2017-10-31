9a59c3d2e3dcdd91055e4c2605825217804226b0
Pavel Shtanko <pavel.shtanko@gmail.com>
2017-10-31 20:09:16 +0100

    Update with some tests

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -48,6 +48,57 @@ func Test_Can_Not_Register_Duplicate(t *testing.T) {
 	}
 }
 
+func Test_Can_Not_Create_User_With_Empty_Email(t *testing.T) {
+	handler := http.HandlerFunc(RegisterHandler)
+
+	request := createRegisterRequest("", "test", "test", "password", "password", t)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, request)
+
+	if rr.Result().StatusCode != http.StatusBadRequest {
+		t.Errorf("Status is incorrect got: %v", rr.Result().StatusCode)
+	}
+}
+
+func Test_Can_Not_Create_User_With_Empty_Password(t *testing.T) {
+	handler := http.HandlerFunc(RegisterHandler)
+
+	request := createRegisterRequest("test1@gmail.com", "test", "test", "", "", t)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, request)
+
+	if rr.Result().StatusCode != http.StatusBadRequest {
+		t.Errorf("Status is incorrect got: %v", rr.Result().StatusCode)
+	}
+}
+
+func Test_Can_Not_Create_User_With_Not_Equal_Passwords(t *testing.T) {
+	handler := http.HandlerFunc(RegisterHandler)
+
+	request := createRegisterRequest("test12@gmail.com", "test", "test", "c", "not equal", t)
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, request)
+
+	if rr.Result().StatusCode != http.StatusBadRequest {
+		t.Errorf("Status is incorrect got: %v", rr.Result().StatusCode)
+	}
+}
+
+func Test_Can_Not_Create_User_With_Wrong_Body(t *testing.T) {
+	handler := http.HandlerFunc(RegisterHandler)
+
+	request, err := http.NewRequest("POST", "/register", bytes.NewBuffer([]byte("")))
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, request)
+
+	if rr.Result().StatusCode != http.StatusBadRequest {
+		t.Errorf("Status is incorrect got: %v", rr.Result().StatusCode)
+	}
+}
+
 func createRegisterRequest(email, firstname, lastname, password, confirmation string, t *testing.T) *http.Request {
 	registerModel := &RegisterModel{
 		Email:                email,
